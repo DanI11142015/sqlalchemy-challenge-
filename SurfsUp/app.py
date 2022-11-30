112c5becf397e90e6b13d83dc8ec79060f15d08a
DanI11142015 <107371332+DanI11142015@users.noreply.github.com>
2022-11-29 19:48:39 -0600

    moved files

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import json 
 
 from flask import Flask, jsonify
 # REMEMBER TO CLOSE SESSIONS AFTER ROUTES
